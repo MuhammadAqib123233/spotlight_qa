389d58825bfbca6fe59c2b4986512413ab5d431a
Muhammad Aqib Akhter <aqibmuhammad418@gmail.com>
2026-03-15 02:18:03 +0500

    Fix Flutter setup for Xcode Cloud

---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+echo "Installing Flutter"
+
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+export PATH="$PATH:`pwd`/flutter/bin"
+
+flutter --version
+
+echo "Getting Flutter packages"
+flutter pub get
+
+echo "Generating iOS build files"
+flutter build ios --no-codesign
+
+cd ios
+pod install
